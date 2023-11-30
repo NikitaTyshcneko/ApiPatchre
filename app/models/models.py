be92494b6a5085232423b92aade092ceb63fd104
Coffe Cat <nikitatyshchenkoofficial@gmail.com>
2023-12-01 01:09:19 +0200

    Remove .pyc files

--- app/models/models.py
+++ app/models/models.py
@@ -3,12 +3,12 @@ from sqlalchemy import Column, Integer, String
 from app.db import Base
 
 
-class Character(Base):
-    __tablename__ = "Character"
+class Card(Base):
+    __tablename__ = "cards"
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     name = Column(String, nullable=False)
     description = Column(String)
     resourceURI = Column(String, nullable=False)
 
 
-character = Character.__tablename__
+character = Card.__tablename__
